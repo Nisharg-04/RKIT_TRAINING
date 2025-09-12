d35d06a1554b5a4785355fab75346fbb9e4320c5
Nisharg Soni <ncsoni9898@gmail.com>
2025-09-12 14:53:49 +0530

    Updated Formats of queries and updated notes

--- Week 1/Assignment/assignment4.sql
+++ Week 1/Assignment/assignment4.sql
@@ -1,16 +1,35 @@
 -- 1. Write a query to fetch all students above age 20.
-SELECT * FROM students WHERE age > 20;
+SELECT 
+		* 
+FROM 
+		students 
+WHERE 
+		age > 20;
 
 -- 2. Fetch all students ordered by name alphabetically.
-SELECT * FROM students ORDER BY name ASC;	
+SELECT 
+		* 
+FROM 
+		students 
+ORDER BY 
+		name ASC;
 
 -- 3. Show total number of students enrolled in each course using GROUP BY.
-SELECT course_id, COUNT(*) AS total_students
-FROM students
-GROUP BY course_id;
+SELECT 
+		course_id, 
+		COUNT(*) AS total_students
+FROM 
+		students
+GROUP BY 
+		course_id;
 
 -- 4. Show courses that have more than 2 students using HAVING
-SELECT course_id, COUNT(*) AS student_count
-FROM students
-GROUP BY course_id
-HAVING student_count > 2;
+SELECT 
+		course_id, 
+		COUNT(*) AS student_count
+FROM 
+		students
+GROUP BY 
+		course_id
+HAVING 
+		student_count > 2;
--- Week 1/Assignment/libraryDB.sql
+++ Week 1/Assignment/libraryDB.sql
@@ -49,38 +49,77 @@ INSERT INTO Borrowers (book_id, borrower_name, borrow_date, return_date) VALUES
 (5, 'Vikas Mehta', '2025-08-15', '2025-08-25');
 
 
+
 -- 1. Retrieve the list of all books along with their author's name.
-SELECT b.title, a.author_name
-FROM Books b
-INNER JOIN Authors a ON b.author_id = a.author_id;
+SELECT 
+	b.title, 
+	a.author_name
+FROM 
+	Books b
+	INNER JOIN 
+	Authors a 
+	ON 
+	b.author_id = a.author_id;
+
 
 -- 2. Find all authors from the India who published books after 2009.
-SELECT a.author_name,b.publish_year
-FROM Authors a
-INNER JOIN Books b ON a.author_id = b.author_id
-WHERE a.country = 'India' AND b.publish_year >2009;
+SELECT 
+	a.author_name,
+	b.publish_year
+FROM 
+	Authors a
+	INNER JOIN 
+	Books b 
+	ON 
+	a.author_id = b.author_id
+WHERE 
+	a.country = 'India' AND b.publish_year >2009;
+
 
 -- 3. List all borrowers who haven’t returned the book yet.
-SELECT borrower_name, borrow_date
-FROM borrowers
-WHERE return_date IS NULL;
+SELECT 
+	borrower_name, 
+	borrow_date
+FROM 
+	borrowers
+WHERE 
+	return_date IS NULL;
+
 
 -- 4. Number of books per author
-SELECT a.author_name, COUNT(b.book_id) AS total_books
-FROM authors a
-LEFT JOIN books b ON a.author_id = b.author_id
-GROUP BY a.author_id;
+SELECT 
+	a.author_name, 
+	COUNT(b.book_id) AS total_books
+FROM 
+	authors a
+	LEFT JOIN 
+	books b 
+	ON 
+	a.author_id = b.author_id
+GROUP BY 
+	a.author_id;
+
 
 -- 5. Average price of books per author
-SELECT a.author_name, AVG(b.price) AS avg_price
-FROM authors a
-INNER JOIN books b ON a.author_id = b.author_id
-GROUP BY a.author_id;
+SELECT 
+	a.author_name, 
+	AVG(b.price) AS avg_price
+FROM 
+	authors a
+	INNER JOIN 
+	books b 
+	ON 
+	a.author_id = b.author_id
+GROUP BY 
+	a.author_id;
 
 ALTER TABLE books
 ADD COLUMN genre VARCHAR(50) NOT NULL DEFAULT 'Uncategorized';
 
-SELECT * FROM books;
+SELECT 
+	* 
+FROM 
+	books;
 
 
 ALTER TABLE books
@@ -104,7 +143,10 @@ ADD CONSTRAINT chk_return_date CHECK (return_date IS NULL OR return_date >= borr
 ALTER TABLE borrowers
 ADD CONSTRAINT chk_email CHECK (borrower_email LIKE '%@%.%');
 
-SELECT * FROM borrowers;
+SELECT 
+	* 
+FROM 
+	borrowers;
 
 UPDATE borrowers SET borrower_email = 'rajesh@example.com', due_date = '2025-08-22' WHERE borrower_id = 1;
 UPDATE borrowers SET borrower_email = 'sneha@example.com', due_date = '2025-08-25' WHERE borrower_id = 2;
@@ -112,42 +154,63 @@ UPDATE borrowers SET borrower_email = 'anil@example.com', due_date = '2025-08-30
 UPDATE borrowers SET borrower_email = 'priya@example.com', due_date = '2025-09-01' WHERE borrower_id = 4;
 UPDATE borrowers SET borrower_email = 'vikas@example.com', due_date = '2025-09-05' WHERE borrower_id = 5;
 
+
 -- 6. Find all books that are currently overdue.
 SELECT
-    br.borrower_name,
-    br.borrower_email,
-    b.title,
-    br.due_date
-FROM borrowers AS br
-JOIN books AS b ON br.book_id = b.book_id
-WHERE br.return_date IS NULL AND br.due_date < CURDATE();
+	br.borrower_name,
+	br.borrower_email,
+	b.title,
+	br.due_date
+FROM 
+	borrowers AS br
+	INNER JOIN 
+	books AS b 
+	ON 
+	br.book_id = b.book_id
+WHERE 
+	br.return_date IS NULL AND br.due_date < CURDATE();
+
 
 -- 7. List all books that have never been borrowed.
-SELECT b.title
-FROM books AS b
-LEFT JOIN borrowers AS br ON b.book_id = br.book_id
-WHERE br.borrower_id IS NULL;
+SELECT 
+	b.title
+FROM 
+	books AS b
+	LEFT JOIN 
+	borrowers AS br 
+	ON 
+	b.book_id = br.book_id
+WHERE 
+	br.borrower_id IS NULL;
+
 
 -- 8. Categorize books by their publication era using a CASE statement.
 SELECT
-    title,
-    publish_year,
-    CASE
-        WHEN publish_year < 1990 THEN 'Classic Era'
-        WHEN publish_year >= 1990 AND publish_year < 2010 THEN 'Modern Era'
-        ELSE 'Contemporary Era'
-    END AS era
-FROM books;
+	title,
+	publish_year,
+	CASE
+	WHEN publish_year < 1990 THEN 'Classic Era'
+	WHEN publish_year >= 1990 AND publish_year < 2010 THEN 'Modern Era'
+	ELSE 'Contemporary Era'
+	END AS era
+FROM 
+	books;
+
 
 -- 9. Find the average number of days a book is borrowed for.
 SELECT
-    AVG(DATEDIFF(return_date, borrow_date)) AS avg_borrow_duration_days
-FROM borrowers
-WHERE return_date IS NOT NULL;
+	AVG(DATEDIFF(return_date, borrow_date)) AS avg_borrow_duration_days
+FROM 
+	borrowers
+WHERE 
+	return_date IS NOT NULL;
+    
 
 -- 10. Create a summary report of how many books were borrowed in each month of 2025.
 SELECT
-    COUNT(CASE WHEN MONTH(borrow_date) = 8 THEN 1 END) AS 'August_Borrows',
-    COUNT(CASE WHEN MONTH(borrow_date) = 9 THEN 1 END) AS 'September_Borrows'
-FROM borrowers
-WHERE YEAR(borrow_date) = 2025;
+	COUNT(CASE WHEN MONTH(borrow_date) = 8 THEN 1 END) AS 'August_Borrows',
+	COUNT(CASE WHEN MONTH(borrow_date) = 9 THEN 1 END) AS 'September_Borrows'
+FROM 
+	borrowers
+WHERE 
+	YEAR(borrow_date) = 2025;
